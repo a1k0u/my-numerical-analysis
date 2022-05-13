0311c3b9a0ab8cafce8b17b24bf7b32597b18ca4
a1k0u <alko7enko@gmail.com>
2022-05-13 11:28:05 +0300

    doc strings

--- interpolation.py
+++ interpolation.py
@@ -1,5 +1,8 @@
 """
-
+Interpolation is a type of estimation,
+a method of constructing new data points
+based on the range of a discrete
+set of known data points.
 """
 
 from typing import List, Union
@@ -7,27 +10,59 @@ from functools import reduce
 
 
 class Interpolation:
-    """ """
+    """
+    Calculate new points by set of known points.
+
+    In this class realised newton
+    and lagrange methods, which
+    calculate new points.
+
+    Firstly, init instance of class.
+    >> x_values = [1, 2, 3]
+    >> y_values = [44, -10, 100]
+    >> function = Interpolation(x_values, y_values)
+
+    After that you are able to call
+    any method (newton, lagrange) by
+    some x point and get f(x).
+
+    >> function.newton(3) # get some float result
+    >> function.lagrange(5)
+
+    >> y_values = [function.newton(x) for x in range(100)]
+    """
 
     def __init__(self, x_values: List[float], y_values: List[float]) -> None:
         """
+        Initialization your function
+        with known data points.
 
-        :param x_values:
-        :param y_values:
+        >> len(x_values) == len(y_values) # True
+        Here's has to be bijection between X and Y.
+
+        :param x_values: list of X values
+        :param y_values: list of f(X) values
         """
+
+        if len(y_values) != len(x_values):
+            raise ValueError("Lengths of arrays must be equal!")
+
         self.__y: List = y_values
         self.__x: List = x_values
 
-    def newton(self, x: Union[int, float]) -> float:
+    def newton(self, point: Union[int, float]) -> float:
+        """
+        Calculate new point by Newton polynomial.
+        Function gets x and returns f(x).
+
+        :param point: find function result at point
+        :return: f(point)
         """
 
-        :param x:
-        :return:
-        """
         polynomial: float = self.__y[0]
         for k in range(len(self.__x) - 1):
             x_values_multiply: float = reduce(
-                lambda a, b: a * b, (x - self.__x[n_i] for n_i in range(k + 1))
+                lambda a, b: a * b, (point - self.__x[n_i] for n_i in range(k + 1))
             )
 
             sep_differences: float = 0
@@ -40,25 +75,31 @@ class Interpolation:
             polynomial += sep_differences * x_values_multiply
         return polynomial
 
-    def lagrange(self, x_point: Union[int, float]) -> float:
+    def lagrange(self, point: Union[int, float]) -> float:
+        """
+        Calculate new point by Lagrange polynomial.
+        Function gets x and returns f(x).
+
+        :param point: find function result at point
+        :return: f(point)
         """
 
-        :param x_point:
-        :return:
-        """
+        def phi_func_calc(value: Union[int, float], index: int) -> float:
+            """
+            Calculate phi function
+            in lagrange polynomial.
 
-        def phi_func_calc(value: float) -> float:
+            :param index: index of loop in lagrange
+            :param value: argument for phi
+            :return: result of phi function
             """
 
-            :param value:
-            :return:
-            """
             return reduce(
                 lambda a, b: a * b,
-                (value - x_j for x_j, j in enumerate(self.__x) if j != i),
+                (value - x_j for x_j, j in enumerate(self.__x) if j != index),
             )
 
         polynomial: float = 0
         for y_i, i in enumerate(self.__y):
-            polynomial += y_i * phi_func_calc(x_point) / phi_func_calc(self.__x[i])
+            polynomial += y_i * phi_func_calc(point, i) / phi_func_calc(self.__x[i], i)
         return polynomial
