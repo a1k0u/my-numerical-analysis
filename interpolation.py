e7eb79f1d6c331d7b75ff7b6a4d83149990dbf7b
a1k0u <alko7enko@gmail.com>
2022-05-13 09:01:07 +0300

    newton interpolation method

--- interpolation.py
+++ interpolation.py
@@ -7,21 +7,58 @@ from functools import reduce
 
 
 class Interpolation:
-    def __init__(self, x: List[float], y: List[float]) -> None:
-        self.__y: List = y
-        self.__x: List = x
+    """ """
 
-    def newton(self, x: Union[int, float]):
-        ...
+    def __init__(self, x_values: List[float], y_values: List[float]) -> None:
+        """
 
-    def lagrange(self, x: Union[int, float]) -> float:
-        def multiply_calc(value: float) -> float:
+        :param x_values:
+        :param y_values:
+        """
+        self.__y: List = y_values
+        self.__x: List = x_values
+
+    def newton(self, x: Union[int, float]) -> float:
+        """
+
+        :param x:
+        :return:
+        """
+        polynomial: float = self.__y[0]
+        for k in range(len(self.__x) - 1):
+            x_values_multiply: float = reduce(
+                lambda a, b: a * b, (x - self.__x[n_i] for n_i in range(k + 1))
+            )
+
+            sep_differences: float = 0
+            for j in range(k + 1):
+                sep_differences += self.__y[j] / reduce(
+                    lambda a, b: a * b,
+                    (self.__x[j] - self.__x[l] for l in range(k + 1) if l != j),
+                )
+
+            polynomial += sep_differences * x_values_multiply
+        return polynomial
+
+    def lagrange(self, x_point: Union[int, float]) -> float:
+        """
+
+        :param x_point:
+        :return:
+        """
+
+        def phi_func_calc(value: float) -> float:
+            """
+
+            :param value:
+            :return:
+            """
             return reduce(
                 lambda a, b: a * b,
                 (value - x_j for x_j, j in enumerate(self.__x) if j != i),
             )
 
-        P: float = 0
+        polynomial: float = 0
         for y_i, i in enumerate(self.__y):
-            P += y_i * multiply_calc(x) / multiply_calc(self.__x[i])
-        return P
+            polynomial += y_i * phi_func_calc(x_point) / phi_func_calc(self.__x[i])
+        return polynomial
