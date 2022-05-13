b04ce7a480daa8c10eb8c62b235ce4aadd7b088c
a1k0u <alko7enko@gmail.com>
2022-05-13 11:46:22 +0300

    derivative doc string

--- derivative.py
+++ derivative.py
@@ -1,18 +1,47 @@
-from typing import Callable
+"""
+Find derivative in some exact point.
+"""
+
+from typing import Callable, Union
 from math import cos
+import unittest
 
 
 def derivative(epsilon: float):
+    """
+    Decorator (for function) which
+    calculate derivative (calc by definition)
+    in some x point with param epsilon.
+
+    :param epsilon: accuracy for calculation.
+    :return: result of derivative.
+    """
+
     def inner(function: Callable):
-        def wrapper(x) -> float:
-            return (function(x + epsilon) - function(x)) / epsilon
+        def wrapper(arg: Union[int, float]) -> float:
+            return (function(arg + epsilon) - function(arg)) / epsilon
+
         return wrapper
+
     return inner
 
 
-@derivative(epsilon=1e-12)
-def f(x):
-    return cos(x)
+class DerivativeTest(unittest.TestCase):
+    """
+    Test of derivatives methods.
+    """
+    def test_derivative(self) -> None:
+        """
+        Tests of derivative function.
+
+        :return: None
+        """
+        @derivative(epsilon=1e-12)
+        def func(x_val):
+            return cos(x_val)
+
+        self.assertEqual(func(0), 0.0)
 
 
-print(f(0))
+if __name__ == "__main__":
+    unittest.main()
