904df0c1aec5180fc3c40793fc22c8a6ca0b9b4e
a1k0u <alko7enko@gmail.com>
2022-06-09 14:55:51 +0300

    process spline

--- spline/main.cpp
+++ spline/main.cpp
@@ -1,18 +1,51 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
 
+#include "smath.h"
 #include "spline.h"
 
 using namespace std;
 
-int main() {
-    vector<POINT> st = {{0, 1}, {2, 10}, {4, -5}, {6, 0}, {8, 11}, {10, 3}};
+int main(int argc, char *argv[]) {
+    vector<FUNCTION *> splines;
+    ifstream fileIn(argv[0]);
 
-    FUNCTION* func1 = createFunction(st);
-    double start = 0, stop = 10, step = 0.5;
-    while (start != stop) {
-        cout << "x=" << start << ", y=" << func1->getY(start) << endl;
-        start += step;
+    while (!fileIn.eof()) {
+        int n;
+        real x, y;
+        vector<POINT> points;
+
+        fileIn >> n;
+        for (int i = 0; i < n; i++) {
+            fileIn >> x >> y;
+            points.push_back({x, y});
+        }
+
+        splines.push_back(createFunction(points));
+    }
+    fileIn.close();
+
+    for (int i = 0; i < (int)splines.size() - 1; ++i) {
+        pair<real, real> fSpln_ = takeMinMaxSplineX(splines[i]);
+        vector<POINT> splineData =
+                buildSplineData(fSpln_.first, fSpln_.second, splines[i]);
+        writeSplineInfoFile("name", splineData);
+
+        for (int j = i + 1; j < (int)splines.size(); ++j) {
+            pair<real, real> sSpln_ = takeMinMaxSplineX(splines[i]);
+            real ROOT = checkRoots(
+                    fSpln_.first >= sSpln_.first ? fSpln_.first : sSpln_.first,
+                    fSpln_.second <= sSpln_.second ? fSpln_.second : sSpln_.second,
+                    splines[i], splines[j]);
+            if (ROOT != nothingRoots) {
+                vector<POINT> point = {{ROOT, splines[i]->getY(ROOT)}};
+                writeSplineInfoFile("name", point);
+            } else {
+                cout << "Nothing roots, min distance: "
+                     << calcMinDistance(0, 10, splines[i], splines[j]);
+            }
+        }
     }
 
     return 0;
