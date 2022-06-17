1dfa415519c4164f13ad658366e69cf36aa151a5
a1k0u <alko7enko@gmail.com>
2022-06-17 19:23:22 +0300

    refactor code

--- tasl.py
+++ tasl.py
@@ -48,9 +48,21 @@ def functions(arguments: np.array, consts: Variables) -> np.array:
     )
 
 
+def make_patch(x: float, angle1: float, angle2: float) -> patches.Arc:
+    return patches.Arc(
+        (x, X_values[2]),
+        X_values[2] * 2,
+        X_values[2] * 2,
+        angle=0,
+        theta1=angle1,
+        theta2=angle2,
+        linewidth=2,
+    )
+
+
 _data = PhysicProperties(pressure=2000, gravity=9.8, mass=100, tau=0.005, epsilon=0.001)
 
-_time = Time(start=0, end=2.5, step=0.01)
+_time = Time(start=0, end=2.65, step=0.01)
 
 _vars = Variables(
     length=0,
@@ -70,7 +82,6 @@ figure, axis = pyplot.subplots()
 camera = Camera(figure)
 
 while _time.start < _time.end:
-
     _vars.Ay = _vars.Ay + _vars.Vy * _time.step
     _vars.By = _vars.Ay
 
@@ -83,42 +94,17 @@ while _time.start < _time.end:
 
     _vars.length = X_values[1] - X_values[0]
 
-    _vars.Vy = (
-        _vars.Vy
-        + (1 / _data.mass)
-        * (_data.pressure * _vars.length - _data.mass * _data.gravity)
-        * _time.step
-    )
-
-    print(X_values, _vars.Ay, _vars.Vy)
-
-    e1 = patches.Arc(
-        (X_values[0], X_values[2]),
-        X_values[2] * 2,
-        X_values[2] * 2,
-        angle=0,
-        theta1=(3 * np.pi / 2 - X_values[3]) * (180 / np.pi),
-        theta2=270,
-        linewidth=2,
-    )
-
-    e2 = patches.Arc(
-        (X_values[1], X_values[2]),
-        X_values[2] * 2,
-        X_values[2] * 2,
-        angle=0,
-        theta1=270,
-        theta2=(3 * np.pi / 2 + X_values[4]) * 180 / np.pi,
-        linewidth=2,
-    )
+    F = _data.pressure * _vars.length - _data.mass * _data.gravity
+    _vars.Vy = _vars.Vy + (1 / _data.mass) * F * _time.step
 
     _time.start += _time.step
 
-    pyplot.text(-0.55, 0.365, "time = %.2f s" % _time.start, size="large")
-    pyplot.text(X_values[0] + 0.025, X_values[2] + 0.025, "x1")
-    pyplot.text(X_values[1] + 0.025, X_values[2] + 0.025, "x2")
-    pyplot.plot(X_values[0], X_values[2], "bo")
-    pyplot.plot(X_values[1], X_values[2], "bo")
+    e1 = make_patch(X_values[0], 270 - X_values[3] * 180 / np.pi, 270)
+    e2 = make_patch(X_values[1], 270, 270 + X_values[4] * 180 / np.pi)
+
+    axis.add_patch(e1)
+    axis.add_patch(e2)
+
     figure.suptitle(
         "Simulation of the movement of a pneumatic balloon",
         fontsize=14,
@@ -129,20 +115,27 @@ while _time.start < _time.end:
         style="italic",
     )
 
-    F = _data.pressure * _vars.length - _data.mass * _data.gravity
+    pyplot.text(-0.55, 0.365, "time = %.2f s" % _time.start, size="large")
+
+    pyplot.text(X_values[0] + 0.025, X_values[2] + 0.025, "x1")
+    pyplot.text(X_values[1] + 0.025, X_values[2] + 0.025, "x2")
+    pyplot.plot(X_values[0], X_values[2], "bo")
+    pyplot.plot(X_values[1], X_values[2], "bo")
+
     direction = 1 if F > 0 else -1
     pyplot.annotate(
-        "F=%.2f H" % (F * direction),
-        xy=((_vars.Ax + _vars.Bx) / 2, _vars.Ay + 0.05 * direction),
-        xytext=((_vars.Ax + _vars.Bx) / 2 - 0.1, _vars.Ay),
+        "F",
+        xy=(
+            (_vars.Ax + _vars.Bx) / 2,
+            _vars.Ay + (abs(F) * 0.000075 + 0.05) * direction,
+        ),
+        xytext=((_vars.Ax + _vars.Bx) / 2 - 0.01, _vars.Ay),
         arrowprops=dict(facecolor="orange", shrink=0.0005, width=3, headwidth=9),
     )
 
     pyplot.xlim([-0.6, 0.6])
     pyplot.ylim([0, 0.4])
 
-    axis.add_patch(e1)
-    axis.add_patch(e2)
     camera.snap()
 
 animation = camera.animate()
